f160857614f8be3f15fb358c5bb7fcdc3e1c732e
alabadoroderickdll-source <alabado.roderick.dll@gmail.com>
2026-05-23 23:34:49 +0800

    Delete event_project directory

--- event_project/events/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import EventRegistration
-
-
-@admin.register(EventRegistration)
-class EventRegistrationAdmin(admin.ModelAdmin):
-    list_display = ['full_name', 'email', 'age', 'registered_at']
-    list_filter = ['registered_at']
-    search_fields = ['full_name', 'email']
